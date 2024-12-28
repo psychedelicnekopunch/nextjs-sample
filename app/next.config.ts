77bcf8cda05e475b951c0b2ac360f316d20583d5
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2024-12-28 13:17:30 +0900

    sass

--- app/next.config.ts
+++ app/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  // https://psychedelicnekopunch.com/archives/3655
+  sassOptions: {
+    silenceDeprecations: ["legacy-js-api"],
+  },
 };
 
 export default nextConfig;
