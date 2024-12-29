247e8250fae47afed3484447849268ed82ddfed5
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2024-12-29 18:04:27 +0900

    fix for build

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -19,13 +19,41 @@ const nextConfig: NextConfig = {
     },
   },
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    })
+    // config.module.rules.push({
+    //   test: /\.svg$/i,
+    //   issuer: /\.[jt]sx?$/,
+    //   use: ["@svgr/webpack"],
+    // })
+
+
+    // https://github.com/vercel/next.js/issues/48177
+    //
+    // Grab the existing rule that handles SVG imports
+    // @ts-ignore - rules is a private property that is not typed
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'))
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      }
+    )
+
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i
+
     return config
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
--- app/src/app/svg/page.tsx
+++ app/src/app/svg/page.tsx
@@ -5,7 +5,7 @@ import Code from "@/components/elements/code"
 import SentimentSatisfied from "@/assets/icon/sentiment_satisfied.svg"
 import Ungroup from "@/assets/icon/ungroup.svg"
 
-export default function Home() {
+export default function Svg() {
   return (
     <main className={styles.main}>
       <div>
@@ -36,11 +36,38 @@ const nextConfig: NextConfig = {
     },
   },
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
-    })
+    // https://github.com/vercel/next.js/blob/canary/examples/svg-components/next.config.js
+    //
+    // config.module.rules.push({
+    //   test: /\.svg$/i,
+    //   issuer: /\.[jt]sx?$/,
+    //   use: ["@svgr/webpack"],
+    // })
+
+
+    // https://github.com/vercel/next.js/issues/48177
+    //
+    const fileLoaderRule = config.module.rules.find((rule) => rule.test?.test?.('.svg'))
+
+    config.module.rules.push(
+      // Reapply the existing rule, but only for svg imports ending in ?url
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      // Convert all other *.svg imports to React components
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      }
+    )
+
+    // Modify the file loader rule to ignore *.svg, since we have it handled now.
+    fileLoaderRule.exclude = /\.svg$/i
+
     return config
   },
 }
