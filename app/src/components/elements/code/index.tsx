b72855ca0608cc8a5fa3c560808db708e8b86315
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2025-01-15 16:41:02 +0900

    update

--- app/src/components/elements/code/index.tsx
+++ app/src/components/elements/code/index.tsx
@@ -6,6 +6,7 @@ import javascript from "highlight.js/lib/languages/javascript"
 import json from "highlight.js/lib/languages/json"
 import typescript from "highlight.js/lib/languages/typescript"
 import plaintext from "highlight.js/lib/languages/plaintext"
+import xml from "highlight.js/lib/languages/xml"
 
 import "highlight.js/styles/monokai.css"
 
@@ -21,16 +22,16 @@ export default function Code({ lang, name, value }: Props) {
 	hljs.registerLanguage("json", json)
 	hljs.registerLanguage("typescript", typescript)
 	hljs.registerLanguage("plaintext", plaintext)
+	hljs.registerLanguage("xml", xml)
 
 	const str = name ? `\n\n${value}` : value
-
 	const highlightedCode = hljs.highlight(str, { language: lang }).value
 
 	return (
 		<div className={styles.code}>
 			{name && <div className={styles.name}>{name}</div>}
 			<pre>
-				<code className={`hljs language-bash`} dangerouslySetInnerHTML={{ __html: highlightedCode }}></code>
+				<code className={`hljs`} dangerouslySetInnerHTML={{ __html: highlightedCode }}></code>
 			</pre>
 		</div>
 	)
