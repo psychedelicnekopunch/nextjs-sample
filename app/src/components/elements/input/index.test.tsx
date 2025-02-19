207964750cc80820237dbc9a197286b68a0c9a6b
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2025-02-19 13:41:51 +0900

    update input

--- app/src/components/elements/input/index.test.tsx
+++ app/src/components/elements/input/index.test.tsx
@@ -1,26 +1,47 @@
 import { describe, jest, expect, test } from "@jest/globals"
 import "@testing-library/jest-dom"
-import { render, fireEvent } from "@testing-library/react"
+import { render, fireEvent, screen } from "@testing-library/react"
 import Input from "./index"
+
+const value = ""
+const onChange = jest.fn()
 
 describe("/components › /elements › <Input>", () => {
 	test("type チェック", () => {
-		const types: string[] = ["text"]
-
-		const value = ""
-		const onChange = (_: React.MouseEvent<HTMLInputElement>) => {}
-
+		const types: string[] = ["text", "file"]
 		for (const [_, type] of types.entries()) {
 			render(<Input type={type} value={value} onChange={onChange} />)
 		}
 	})
 
 	test("onChange() が呼ばれるかどうか", () => {
-		const onChange = jest.fn()
-
-		const value = ""
-		const { getByTestId } = render(<Input value={value} onChange={onChange} />)
-		fireEvent.change(getByTestId("input"), { target: { value: "v" } })
+		render(<Input value={value} onChange={onChange} />)
+		fireEvent.change(screen.getByTestId("input"), { target: { value: "v" } })
 		expect(onChange).toBeCalled()
+	})
+
+	// test("ファイルアップロード →  onChange()", () => {
+
+	// 	const value = ""
+	// 	const onChange = jest.fn()
+	// 	render(<Input type="file" value={value} onChange={onChange} />)
+	// 	fireEvent.click(screen.getByTestId("input"), { dataTransfer: {
+	// 		files: [
+	// 			new File(["(⌐□_□)"], "chucknorris.png", {type: "image/png"})
+	// 		],
+	// 	}})
+	// // 	console.log(getByTestId("input").style.display)
+	// 	expect(onChange).toBeCalled()
+	// })
+
+	// test("onCancel() が呼ばれるかどうか", () => {
+	// })
+
+	test("type が file の場合 label が存在しているか、input の id と label の htmlFor が一致しているかチェックする", () => {
+		render(<Input type="file" value={value} onChange={onChange} />)
+		expect(screen.getByTestId("label")).toBeVisible()
+		// expect(screen.getByTestId("label")).toBeTruthy()
+		// expect(screen.getByTestId("input")).not.toBeVisible()
+		expect(screen.getByTestId("label").htmlFor).toBe(screen.getByTestId("input").id)
 	})
 })
--- app/src/components/elements/input/index.tsx
+++ app/src/components/elements/input/index.tsx
@@ -1,13 +1,42 @@
+"use client"
+
+// import { useEffect as UseEffect } from "react"
+import { useId } from "react"
 import styles from "./styles.module.scss"
 
 type Props = {
 	value: string
 	type?: Type
 	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+	// onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
+	// onCancel?: (e: React.CancelEvent<HTMLInputElement>) => void
+	// onCancel?: (e: Event) => void
 }
 
-type Type = "text"
+type Type = "text" | "file"
 
-export default function Input({ value, type = "text", onChange }: Props) {
-	return <input data-testid="input" className={styles.input} type={type} value={value} onChange={onChange} />
+export default function Input({ value, type = "text", onChange = () => {} }: Props) {
+	const id: string = useId()
+
+	// function init() {
+	// 	const target = document.getElementById(id)
+	// 	if (target) {
+	// 		target.addEventListener("cancel", onCancel)
+	// 	}
+	// }
+
+	// UseEffect(() => {
+	// 	init()
+	// }, [id])
+
+	return (
+		<>
+			{type == "file" && (
+				<label htmlFor={id} data-testid="label" className={styles["label-upload"]}>
+					ファイルを選択
+				</label>
+			)}
+			<input id={id} data-testid="input" className={styles.input} type={type} value={value} onChange={onChange} />
+		</>
+	)
 }
