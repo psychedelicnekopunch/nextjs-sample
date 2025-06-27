0d73657239f0f82a361469462cb086e46007c535
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2025-06-27 16:33:27 +0900

    fix

--- app/src/infrastructure/cookie.ts
+++ app/src/infrastructure/cookie.ts
@@ -2,6 +2,9 @@
 
 import { cookies } from "next/headers"
 
+// https://nextjs.org/docs/app/api-reference/functions/cookies#options
+// You can use the (await cookies()).set(name, value, options) method in a Server Action or Route Handler to set a cookie.
+//
 export async function create(name: string, value: string) {
 	;(await cookies()).set(name, value, {
 		path: "/",
